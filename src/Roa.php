065c836a86c658f62efaa53439294c5e984696dd
xuchao <85825770@qq.com>
2017-03-28 17:50:00 +0800

    add Roa

---
+++ src/Roa.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace xutl\guzzle\subscriber\aliyun;
+
+use Psr\Http\Message\RequestInterface;
+
+class Poa
+{
+    /** @var array Configuration settings */
+    private $config;
+
+    public function __construct($config)
+    {
+        $this->config = [
+            'Version' => '2016-11-01',
+            'accessKeyId' => '123456',
+            'accessSecret' => '654321',
+            'signatureMethod' => 'HMAC-SHA1',
+            'signatureVersion' => '1.0',
+            'dateTimeFormat' => 'Y-m-d\TH:i:s\Z',
+        ];
+        foreach ($config as $key => $value) {
+            $this->config[$key] = $value;
+        }
+    }
+
+    /**
+     * Called when the middleware is handled.
+     *
+     * @param callable $handler
+     *
+     * @return \Closure
+     */
+    public function __invoke(callable $handler)
+    {
+        return function ($request, array $options) use ($handler) {
+            //暂未实现
+            return $request;
+        };
+    }
+}
