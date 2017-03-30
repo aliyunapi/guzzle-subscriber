4fb77db390fff35d3565e50f8e33da82e7ba4189
xuchao <85825770@qq.com>
2017-03-30 12:04:58 +0800

    ROA 部分实现

--- src/Roa.php
+++ src/Roa.php
@@ -14,6 +14,8 @@ class Roa
     /** @var array Configuration settings */
     private $config;
 
+    private static $headerSeparator = "\n";
+
     public function __construct($config)
     {
         $this->config = [
@@ -39,8 +41,70 @@ class Roa
     public function __invoke(callable $handler)
     {
         return function ($request, array $options) use ($handler) {
-            //暂未实现
-            return $request;
+            $request = $this->onBefore($request);
+            return $handler($request, $options);
         };
+    }
+
+    /**
+     * 请求前调用
+     * @param RequestInterface $request
+     * @return RequestInterface
+     */
+    private function onBefore(RequestInterface $request)
+    {
+        $headers = $request->getHeaders();
+
+        $headers["Date"] = gmdate($this->config['dateTimeFormat']);
+        $headers["Accept"] = 'application/octet-stream';
+        $headers["x-acs-signature-method"] = $this->config['signatureMethod'];
+        $headers["x-acs-signature-version"] = $this->config['signatureVersion'];
+
+        if(isset($this->config['regionId'])){
+            $headers["x-acs-region-id"] = $this->config['regionId'];
+        }
+
+        $content = $request->getBody()->getContents();
+        if ($content != null) {
+            $headers["Content-MD5"] = base64_encode(md5(json_encode($content),true));
+        }
+        $headers["Content-Type"] = "application/octet-stream;charset=utf-8";
+
+
+        $signString = $request->getMethod() . self::$headerSeparator;
+        if (isset($headers["Accept"])) {
+            $signString = $signString . $headers["Accept"];
+        }
+        $signString = $signString . self::$headerSeparator;
+
+        if (isset($headers["Content-MD5"])) {
+            $signString = $signString . $headers["Content-MD5"];
+        }
+        $signString = $signString . self::$headerSeparator;
+
+        if (isset($headers["Content-Type"])) {
+            $signString = $signString . $headers["Content-Type"];
+        }
+        $signString = $signString . self::$headerSeparator;
+
+        if (isset($headers["Date"])) {
+            $signString = $signString . $headers["Date"];
+        }
+        $signString = $signString . self::$headerSeparator;
+
+
+        //etc....
+        return $request;
+        $uri = $this->replaceOccupiedParameters();
+        $signString = $signString.$this->buildCanonicalHeaders();
+        $queryString = $this->buildQueryString($uri);
+        $signString .= $queryString;
+
+        $requestUrl = $request->getProtocol()."://".$domain.$queryString;
+        return $requestUrl;
+
+        $request = $request->withHeader('Authorization', "acs ".$this->config['accessKeyId'].":"
+            .$iSigner->signString($signString, $this->config['accessSecret']));
+        return $request;
     }
 }
